d8b6cca99d90596662ede9ed1214dc3110d89721
Dean Camera <dean@fourwalledcubicle.com>
2013-09-15 01:21:21 +0200

    Clean up instrumentation code.

--- StackChecker/Resources/InstrumentationCode.c
+++ StackChecker/Resources/InstrumentationCode.c
@@ -8,6 +8,10 @@
 
 #include <stdint.h>
 
+#ifndef __GNUC__
+#  error  The stack instrumentation code is designed for GCC toolchains only.
+#endif
+
 /** Linker provided symbols for the end of the static data section, and the
  *  of the stack.
  */
@@ -16,12 +20,12 @@ extern void *_end, *__stack;
 /** \internal
  *  \brief Low level stack painting function, hooked into the avr-libc initialization code.
  *
- *  This function paints the internal SRAM between the end of the static data
- *  and the start of the stack with a known \c 0xDC hex constant. This is then
- *  detected by the Stack Checker extension when a debug session is halted to
- *  determine the maximim stack usage of the running application.
+ *  Paints the internal SRAM between the end of the static data and the start
+ *  of the stack with a known \c 0xDC hex constant. This is then detected by
+ *  the Stack Checker extension when a debug session is halted to determine the
+ *  maximum stack usage of the running application.
  */
-void _StackPaint(void) __attribute__ ((naked)) __attribute__ ((section (".init1")));
+void _StackPaint(void) __attribute__((naked)) __attribute__((section (".init1")));
 void _StackPaint(void)
 {
 	uint8_t *p = (uint8_t*)&_end;
