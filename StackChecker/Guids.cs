89ee9bbf69a7998f7f6166c69aa24dbfa8ed5445
Dean Camera <dean@fourwalledcubicle.com>
2013-09-14 21:24:18 +0200

    Initial commit.

---
+++ StackChecker/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace FourWalledCubicle.StackChecker
+{
+    static class GuidList
+    {
+        public const string guidStackCheckerPkgString = "fbc19046-ee56-4aa8-be8e-3998149b311a";
+        public const string guidStackCheckerCmdSetString = "7f97388a-6dbc-4f23-917c-4d70941c1a78";
+        public const string guidToolWindowPersistanceString = "dc087184-1ea4-4848-8ef6-772ecf0ac118";
+
+        public static readonly Guid guidStackCheckerCmdSet = new Guid(guidStackCheckerCmdSetString);
+    };
+}
---
+++ StackChecker/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace FourWalledCubicle.StackChecker
+{
+    static class PkgCmdIDList
+    {
+
+        public const uint cmdidStackChecker =    0x101;
+
+    };
+}
