e2035919c71f1bf8fd93e3b57a3eb7e282c20f76
Dean Camera <dean@fourwalledcubicle.com>
2013-09-15 01:21:53 +0200

    Ensure missing services/contexts will not crash the IDE.

--- StackChecker/StackCheckerWindow.xaml.cs
+++ StackChecker/StackCheckerWindow.xaml.cs
@@ -28,75 +28,6 @@ namespace FourWalledCubicle.StackChecker
             mTargetService = ATServiceProvider.TargetService2;
         }
 
-        void mDebuggerEvents_OnEnterRunMode(dbgEventReason Reason)
-        {
-            stackUsageProgress.Maximum = 100;
-            stackUsageProgress.Value = 0;
-            deviceName.Text = "(Break execution to refresh)";
-            stackUsageVal.Text = "(Break execution to refresh)";
-        }
-
-        void mDebuggerEvents_OnEnterBreakMode(dbgEventReason Reason, ref dbgExecutionAction ExecutionAction)
-        {
-            stackUsageProgress.Maximum = 100;
-            stackUsageProgress.Value = 0;
-            deviceName.Text = "N/A";
-            stackUsageVal.Text = "N/A";
-
-            ITarget2 target = mTargetService.GetLaunchedTarget();
-            IAddressSpace internalSRAMSpace;
-            IMemorySegment internalSRAMSegment;
-            if (GetInternalSRAM(target, out internalSRAMSpace, out internalSRAMSegment))
-            {
-                stackUsageProgress.Maximum = internalSRAMSegment.Size;
-                stackUsageProgress.Value = GetMaximumStackUsage(target, internalSRAMSpace, internalSRAMSegment);
-
-                deviceName.Text = target.Device.Name;
-                stackUsageVal.Text = string.Format("{0}/{1} ({2}%)",
-                    stackUsageProgress.Value.ToString(), stackUsageProgress.Maximum.ToString(),
-                    Math.Min(100, Math.Ceiling((100.0 * stackUsageProgress.Value) / stackUsageProgress.Maximum)));
-            }
-        }
-
-        ulong GetMaximumStackUsage(ITarget2 target, IAddressSpace addressSpace, IMemorySegment memorySegment)
-        {
-            MemoryErrorRange[] errorRange;
-            byte[] result = target.GetMemory(
-                target.GetAddressSpaceName(addressSpace.Name),
-                memorySegment.Start, 1, (int)memorySegment.Size, 0, out errorRange);
-
-            for (int i = (result.Length - 1); i >= 0; i -= 4)
-            {
-                if ((result[i - 0] == 0xDC) && (result[i - 1] == 0xDC) && (result[i - 2] == 0xDC) && (result[i - 3] == 0xDC))
-                {
-                    return (memorySegment.Size - (ulong)i);
-                }
-            }
-
-            return memorySegment.Size;
-        }
-
-        bool GetInternalSRAM(ITarget2 target, out IAddressSpace addressSpace, out IMemorySegment memorySegment)
-        {
-            foreach (IAddressSpace a in target.Device.AddressSpaces)
-            {
-                foreach (IMemorySegment s in a.MemorySegments)
-                {
-                    if ((s.Type.IndexOf("RAM", StringComparison.OrdinalIgnoreCase) >= 0) &&
-                        (s.Name.IndexOf("INTERNAL", StringComparison.OrdinalIgnoreCase) >= 0))
-                    {
-                        addressSpace = a;
-                        memorySegment = s;
-                        return true;
-                    }
-                }
-            }
-
-            addressSpace = null;
-            memorySegment = null;
-            return false;
-        }
-
         private void addInstrumentCode_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             string startupProjectName = string.Empty;
@@ -121,6 +52,87 @@ namespace FourWalledCubicle.StackChecker
                 }
                 catch { }
             }
+        }
+
+        void mDebuggerEvents_OnEnterRunMode(dbgEventReason Reason)
+        {
+            stackUsageProgress.Maximum = 100;
+            stackUsageProgress.Value = 0;
+            deviceName.Text = "(Break execution to refresh)";
+            stackUsageVal.Text = "(Break execution to refresh)";
+        }
+
+        void mDebuggerEvents_OnEnterBreakMode(dbgEventReason Reason, ref dbgExecutionAction ExecutionAction)
+        {
+            stackUsageProgress.Maximum = 100;
+            stackUsageProgress.Value = 0;
+            deviceName.Text = "N/A";
+            stackUsageVal.Text = "N/A";
+
+            Dispatcher.Invoke(new Action(() => UpdateStackUsageInfo()));
+        }
+
+        void UpdateStackUsageInfo()
+        {
+            ITarget2 target = mTargetService.GetLaunchedTarget();
+            if (target == null)
+                return;
+
+            IAddressSpace internalSRAMSpace;
+            IMemorySegment internalSRAMSegment;
+            if (GetInternalSRAM(target, out internalSRAMSpace, out internalSRAMSegment))
+            {
+                stackUsageProgress.Maximum = internalSRAMSegment.Size;
+                stackUsageProgress.Value = GetMaximumStackUsage(target, internalSRAMSpace, internalSRAMSegment);
+
+                deviceName.Text = target.Device.Name;
+                stackUsageVal.Text = string.Format("{0}/{1} ({2}%)",
+                    stackUsageProgress.Value.ToString(), stackUsageProgress.Maximum.ToString(),
+                    Math.Min(100, Math.Ceiling((100.0 * stackUsageProgress.Value) / stackUsageProgress.Maximum)));
+            }
+        }
+
+        ulong GetMaximumStackUsage(ITarget2 target, IAddressSpace addressSpace, IMemorySegment memorySegment)
+        {
+            try
+            {
+                MemoryErrorRange[] errorRange;
+                byte[] result = target.GetMemory(
+                    target.GetAddressSpaceName(addressSpace.Name),
+                    memorySegment.Start, 1, (int)memorySegment.Size, 0, out errorRange);
+
+                for (int i = (result.Length - 1); i >= 0; i -= 4)
+                {
+                    if ((result[i - 0] == 0xDC) && (result[i - 1] == 0xDC) && (result[i - 2] == 0xDC) && (result[i - 3] == 0xDC))
+                    {
+                        return (memorySegment.Size - (ulong)i);
+                    }
+                }
+            }
+            catch { }
+
+            return memorySegment.Size;
+        }
+
+        bool GetInternalSRAM(ITarget2 target, out IAddressSpace addressSpace, out IMemorySegment memorySegment)
+        {
+            foreach (IAddressSpace a in target.Device.AddressSpaces)
+            {
+                foreach (IMemorySegment s in a.MemorySegments)
+                {
+                    if ((s.Type.IndexOf("RAM", StringComparison.OrdinalIgnoreCase) >= 0) &&
+                        (s.Name.IndexOf("INTERNAL", StringComparison.OrdinalIgnoreCase) >= 0))
+                    {
+                        addressSpace = a;
+                        memorySegment = s;
+                        return true;
+                    }
+                }
+            }
+
+            addressSpace = null;
+            memorySegment = null;
+            return false;
         }
     }
 }
