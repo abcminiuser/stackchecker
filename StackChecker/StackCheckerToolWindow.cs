b3721a32def38b631a3479e617c887d38edbfdd9
Dean Camera <dean@fourwalledcubicle.com>
2013-09-14 21:27:31 +0200

    Rename source files to match the project name.

--- StackChecker/StackCheckerToolWindow.cs
+++ StackChecker/StackCheckerToolWindow.cs
@@ -10,22 +10,10 @@ using Microsoft.VisualStudio.Shell;
 
 namespace FourWalledCubicle.StackChecker
 {
-    /// <summary>
-    /// This class implements the tool window exposed by this package and hosts a user control.
-    ///
-    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane, 
-    /// usually implemented by the package implementer.
-    ///
-    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its 
-    /// implementation of the IVsUIElementPane interface.
-    /// </summary>
     [Guid("dc087184-1ea4-4848-8ef6-772ecf0ac118")]
-    public class MyToolWindow : ToolWindowPane
+    public class StackCheckerToolWindow : ToolWindowPane
     {
-        /// <summary>
-        /// Standard constructor for the tool window.
-        /// </summary>
-        public MyToolWindow() :
+        public StackCheckerToolWindow() :
             base(null)
         {
             // Set the window title reading it from the resources.
@@ -41,7 +29,7 @@ namespace FourWalledCubicle.StackChecker
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on 
             // the object returned by the Content property.
-            base.Content = new MyControl();
+            base.Content = new StackCheckerWindow();
         }
     }
 }
