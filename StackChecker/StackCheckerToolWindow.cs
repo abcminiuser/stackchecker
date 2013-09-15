d522036afcbe0ae972336fc4e99e90ef4fa633cf
Dean Camera <dean@fourwalledcubicle.com>
2013-09-15 09:52:56 +0200

    Add tool window icon.

--- StackChecker/StackCheckerToolWindow.cs
+++ StackChecker/StackCheckerToolWindow.cs
@@ -24,7 +24,7 @@ namespace FourWalledCubicle.StackChecker
             // while the Index is the offset in the bitmap strip. Each image in
             // the strip being 16x16.
             this.BitmapResourceID = 301;
-            this.BitmapIndex = 1;
+            this.BitmapIndex = 0;
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on 
